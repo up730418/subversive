e20271178ede28d28f996cbab6965440157d92c9
up730418 <up730418@myport.ac.uk>
2018-05-16 13:42:48 +0100

    Websocet and second screen implemetation

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const PythonShell = require('python-shell');
 const express = require('express');
 const app = express();
 const port = 8080;
-var fs = require('fs');
-var lame = require('lame');
-var Speaker = require('speaker');
+const fs = require('fs');
+const lame = require('lame');
+const Speaker = require('speaker');
+const WebSocketServer = require("ws").Server
+const wss = new WebSocketServer({port:3434});
 
 let options = {
 	  mode: 'text',
@@ -38,14 +40,30 @@ app.get('/servoAntiClock', async (req, res) => {
   });
 });
 
-app.get('/speak', async (req, res) => {
+app.get('/alarm', async (req, res) => {
   //sudo apt-get install libasound2-dev (for speaker library to work)
-	fs.createReadStream('sounds/UptownFunk.mp3')
+	fs.createReadStream('sounds/alarm.mp3')
 	  .pipe(new lame.Decoder())
 	  .on('format', function (format) {
 		      this.pipe(new Speaker(format));
 		    });
 	res.sendStatus(200);
+});
+
+app.get('/elevator', async (req, res) => {
+  //sudo apt-get install libasound2-dev (for speaker library to work)
+	fs.createReadStream('sounds/elevator.mp3')
+	  .pipe(new lame.Decoder())
+	  .on('format', function (format) {
+		      this.pipe(new Speaker(format));
+		    });
+	res.sendStatus(200);
+});
+
+app.get('/screen', async (req, res) => {
+  //sudo apt-get install libasound2-dev (for speaker library to work)
+  wss.broadcast('change', 'screen')
+  res.sendStatus(200);
 });
 
 app.use(express.static('static'));
@@ -54,7 +72,14 @@ app.listen(port, (err) => {
   if (err) {
     console.error('error', err);
    } else {
-      console.error(`app listening on port ${port}`);
+    console.log(`app listening on port ${port}`);
 					      }
 });
 
+wss.broadcast = function broadcast(message, room){
+	wss.clients.forEach(function(client){ 
+		if(room == client.protocol){
+			client.send(JSON.stringify([{message: message}]))
+		}
+	});
+};
--- static/storyline.js
+++ static/storyline.js
@@ -1,17 +1,22 @@
 function serverCommand(command) {
-  fetch('http://' + window.location.hostname + ':8080/' + command)
+  fetch('http://' + window.location.hostname + ':8080/' + command);
 }
 
 window.addEventListener('load', () => {
   
   turnOnAlarm.addEventListener('click', ()=> {
-    serverCommand("speak");
-  })
-  elevator.addEventListener('click', ()=> {
+    serverCommand("alarm");
+  });
+  
+  elevatorOn.addEventListener('click', ()=> {
     serverCommand("servoClock");
-  })
+    serverCommand("servoClock");
+    serverCommand("elevator");
+  });
 //  servoAnti.addEventListener('click', ()=> {
 //    serverCommand("servoAntiClock");
 //  })
-  liveCamera.src = 'http://' + window.location.hostname + ':8300/image.jpg'
-})
+  if(document.getElementById("liveCamera")){
+    liveCamera.src = 'http://' + window.location.hostname + ':8300/image.jpg';
+  }
+});
